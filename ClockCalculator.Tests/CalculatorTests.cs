c136fbf9df084420b15728bbac508648812ffa1f
Dale Palmer <dalepalmer@outlook.com>
2018-08-28 21:27:56 +0100

    Calculating hours

---
+++ ClockCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ClockCalculator.Tests
+{
+    public class CalculatorTests
+    {
+        [TestCase(3, 90)]
+        [TestCase(6, 180)]
+        public void Calculator_GetDegrees_OnTheHour_ReturnsCorrectAngle(int hour, int expected)
+        {
+            // Assemble
+            var sut = new Calculator();
+
+            // Act
+            var actual = sut.GetDegrees(hour, 0);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ClockCalculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClockCalculator
+{
+    public class Calculator
+    {
+        private const int DEGREES_PER_HOUR = 30;
+
+        public int GetDegrees(int hour, int minutes)
+        {
+            return hour * DEGREES_PER_HOUR;
+        }
+    }
+}
